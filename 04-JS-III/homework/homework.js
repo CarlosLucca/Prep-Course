7f1aa8357807be3db1d3591744069341c150e8ce
Carlos Lucca <ECLL6447@itau.cl>
2022-05-04 16:24:32 -0300

    new changes

--- 04-JS-III/homework/homework.js
+++ 04-JS-III/homework/homework.js
@@ -83,6 +83,16 @@ function arrayContiene(array, elemento) {
   // Comprueba si el elemento existe dentro de "array"
   // Devuelve "true" si está, o "false" si no está
   // Tu código:
+  /*for (var i = 0; i < array.length; i++) {
+    if (array[i]=== elemento) {
+      return true;
+    }
+  }
+  return false;*/
+
+  if (array.includes(elemento)) {
+    return true;
+  }else return false;
 }
 
 
@@ -90,6 +100,16 @@ function agregarNumeros(numeros) {
   // "numeros" debe ser un arreglo de enteros (int/integers)
   // Suma todos los enteros y devuelve el valor
   // Tu código:
+  /*
+    let sumaElementos = 0;
+
+    for (let i = 0; i < numeros.length; i++) {
+        sumaElementos += numeros[i];
+    }
+  */  
+    var reducer = (accumulator, curr) => accumulator + curr;
+    return(numeros.reduce(reducer));
+
 }
 
 
@@ -97,6 +117,11 @@ function promedioResultadosTest(resultadosTest) {
   // "resultadosTest" debe ser una matriz de enteros (int/integers)
   // Itera (en un bucle) los elementos del array, calcula y devuelve el promedio de puntajes
   // Tu código:
+  let count = 0
+  for (let index = 0; index < resultadosTest.length; index++) {
+    count = count + resultadosTest[index];    
+  }
+  return count / resultadosTest.length;
 }
 
 
@@ -104,6 +129,13 @@ function numeroMasGrande(numeros) {
   // "numeros" debe ser una matriz de enteros (int/integers)
   // Devuelve el número más grande
   // Tu código:
+  let mayorNumero = 0;
+  for (let index = 0; index < numeros.length; index++) {
+   if (numeros[index] >= mayorNumero ) {
+     mayorNumero = numeros[index];
+    }
+  }
+  return mayorNumero;
 }
 
 
@@ -111,13 +143,27 @@ function multiplicarArgumentos() {
   // Usa la palabra clave `arguments` para multiplicar todos los argumentos y devolver el producto
   // Si no se pasan argumentos devuelve 0. Si se pasa un argumento, simplemente devuélvelo
   // Escribe tu código aquí:
+let numerobase = 1
+if  (arguments.length === 0){ 
+    return 0;
+}else{
+  		for (let index = 0; index < arguments.length; index++) {
+				numerobase = numerobase * arguments[index];
+      }
+	}
 }
 
 
 function cuentoElementos(arreglo){
   //Realiza una función que retorne la cantidad de los elementos del arreglo cuyo valor es mayor a 18.
   //Escribe tu código aquí
-
+ let count = 0;
+  for (let index = 0; index < arreglo.length; index++) {
+    if (arreglo[index] > 18) {
+      count += 1;
+    }   
+  } 
+ return count;
 }
 
 
@@ -126,7 +172,18 @@ function diaDeLaSemana(numeroDeDia) {
   //Realiza una función que dado el número del día de la semana, retorne: Es fin de semana
   //si el día corresponde a Sábado o Domingo y “Es dia Laboral” en caso contrario. 
   //Escribe tu código aquí   
-  
+  /*1 = Domingo, 2 = Lunes, 3 = Martes, 4 = Miercoles, 5 = Jueves, 6 = Viernes, 7 = Sabado  */ 
+  switch (numeroDeDia) {
+    case 1:
+      return 'Es fin de semana'
+      break;
+    case 7:
+      return 'Es fin de semana'
+      break;
+    default:
+      return 'Es dia Laboral'
+      break;
+  }
 } 
 
 
@@ -134,6 +191,9 @@ function empiezaConNueve(n) {
   //Desarrolle una función que recibe como parámetro un número entero n. Debe retornar true si el entero 
   //inicia con 9 y false en otro caso.
   //Escribe tu código aquí
+  if (n.toString().startsWith(9)) {
+    return true;
+  }return false;
   
 }
 
@@ -142,6 +202,19 @@ function todosIguales(arreglo) {
   //Escriba la función todosIguales, que indique si todos los elementos de un arreglo son iguales:
   //retornar true, caso contrario retornar false.
   //Escribe tu código aquí  
+  let countduplicate = 1
+  let igualNumero = 0;
+  for (let index = 0; index < arreglo.length; index++) {
+    if (arreglo[index] === igualNumero ) { 
+      countduplicate += 1;
+    }else igualNumero = arreglo[index];
+  }
+
+  if (arreglo.length === countduplicate) {
+    return true
+  }else return false;
+
+
   
 } 
 
@@ -151,6 +224,23 @@ function mesesDelAño(array) {
   // "Enero", "Marzo" y "Noviembre", guardarlo en nuevo array y retornarlo.
   //Si alguno de los meses no está, devolver: "No se encontraron los meses pedidos"
   // Tu código:
+  let arraySearch = [];
+  for (let index = 0; index < array.length; index++) {
+     switch (array[index]) {
+       case "Noviembre":
+         arraySearch.push(array[index]);
+        break;
+       case "Marzo":
+          arraySearch.push(array[index]);
+        break;
+       case "Enero":
+          arraySearch.push(array[index]);
+       break;
+     }
+  }
+  if ((arraySearch.includes("Noviembre")) && (arraySearch.includes("Enero")) && (arraySearch.includes("Marzo")) ) {
+    return arraySearch;
+  }else return 'No se encontraron los meses pedidos';
 }
 
 
@@ -158,6 +248,14 @@ function mayorACien(array) {
   //La función recibe un array con enteros entre 0 y 200. Recorrer el array y guardar en un nuevo array sólo los
   //valores mayores a 100 (no incluye el 100). Finalmente devolver el nuevo array.
   // Tu código:
+
+  let arrayHigher = [];
+  for (let index = 0; index < array.length; index++) {
+     if (array[index] > 100) {
+       arrayHigher.push(array[index]);
+     }
+  }
+  return arrayHigher;
 }
 
 
@@ -169,6 +267,18 @@ function breakStatement(numero) {
   //devolver: "Se interrumpió la ejecución"
   //Pista: usá el statement 'break'
   // Tu código:
+  let newArray = [];
+  let countNewNumber = numero;
+  for (let index = 0; index <= 9; index++) {
+    countNewNumber = (countNewNumber + 2)     
+    newArray.push(countNewNumber);
+    if (index === countNewNumber) {
+      return 'Se interrumpió la ejecución';
+      break;
+    }
+
+  }
+  return newArray;
 }
 
 
@@ -179,6 +289,18 @@ function continueStatement(numero) {
   //Cuando el número de iteraciones alcance el valor 5, no se suma en ese caso y se continua con la siguiente iteración
   //Pista: usá el statement 'continue'
   // Tu código:
+  let newArray = [];
+  let countNewNumber = numero;
+  for (let index = 0; index <= 9; index++) {
+    if (index === 5) {
+      continue;
+    }else{
+      countNewNumber = (countNewNumber + 2)     
+      newArray.push(countNewNumber);
+    }
+
+  }
+  return newArray;
 }
 
 
